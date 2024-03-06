12d4e7125635b0ddab10ddd7c846c7c3f1b1b5ff
Oyanghd <885299886@qq.com>
2024-03-06 22:16:45 +0800

    feat(test, interfaces): feat Linux posix test and app to cli interfaces

---
+++ test/test_linux_shmem.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SHM_NAME "/my_shared_memory"
+#define SHM_SIZE 1024  // 1KB
+
+int main() {
+    int shm_fd;
+    void *ptr;
+
+    // 创建共享内存
+    shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // 设置共享内存大小
+    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
+        perror("ftruncate");
+        exit(EXIT_FAILURE);
+    }
+
+    // 映射共享内存
+    ptr = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (ptr == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    // 使用共享内存
+    sprintf((char*)ptr, "Hello, shared memory!");
+
+    // 取消映射共享内存
+    if (munmap(ptr, SHM_SIZE) == -1) {
+        perror("munmap");
+        exit(EXIT_FAILURE);
+    }
+
+
+    // 关闭共享内存文件描述符
+    if (close(shm_fd) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("success");
+    return 0;
+}
